5dd602df3088563ef1f7bc777ece76026090eddd
RoeiGida <roeiroei77777@gmail.com>
2021-06-09 15:08:30 +0300

    case 8 base

--- ex1.py
+++ ex1.py
@@ -13,6 +13,9 @@ from cryptography.hazmat.primitives.serialization import load_pem_public_key, lo
 user_input = ""
 merkle_tree = []
 
+sparse_tree = []
+default_hash = []
+
 
 class Node:
 
@@ -23,6 +26,19 @@ class Node:
 def case1():
     global merkle_tree, user_input
     merkle_tree.append(Node(user_input[0]))
+
+def myHash(value):
+    hasher = hashlib.sha256()
+    hasher.update(value.encode())
+    return hasher.hexdigest()
+
+def init_default_levels():
+    global default_hash
+    default_leaf_val = "0"
+    for i in range(0, 257):
+        default_hash.append(default_leaf_val)
+        default_leaf_val = str(default_leaf_val) + str(default_leaf_val)
+        default_leaf_val = hashlib.sha256(default_leaf_val.encode('utf-8')).hexdigest()
 
 
 def root_calc():
@@ -154,11 +170,45 @@ def case7():
 
 
 def case8():
-    print("8")
+    global sparse_tree, user_input
+    sparse_tree.append(int(user_input[0], 16))
+
+
+def split_list(arr, index):
+    i = 0
+    for x in arr:
+        if x > index:
+            return arr[:i], arr[i:]
+        i += 1
+    return arr[:], []
+
+
+def find_root(arr, level, start_index):
+    if level == 255:
+        if (start_index + 1) in arr:
+            hashed_right = hashlib.sha256("1".encode('utf-8')).hexdigest()
+        else:
+            hashed_right = default_hash[0]
+        if start_index in arr:
+            hashed_left = hashlib.sha256("1".encode('utf-8')).hexdigest()
+        else:
+            hashed_left = default_hash[0]
+        return hashlib.sha256((hashed_right + hashed_left).encode('utf-8')).hexdigest()
+    if len(arr) == 0:
+        return default_hash[255-level]
+    left_arr, right_arr = split_list(arr, (start_index + 2 ** 256 / 2 ** (level + 1)) - 1)
+    hashed_right = find_root(right_arr, level+1, start_index + 2 ** 256 / 2 ** (level + 1))
+    hashed_left = find_root(left_arr, level+1, start_index)
+    return hashlib.sha256((hashed_left + hashed_right).encode('utf-8')).hexdigest()
 
 
 def case9():
-    print("9")
+    global sparse_tree
+    if len(sparse_tree) == 0:
+        print(default_hash[-1])
+    else:
+        sparse_tree.sort()
+        print(find_root(sparse_tree, 0, 0))
 
 
 def case10():
@@ -171,6 +221,7 @@ def case11():
 
 # Press the green button in the gutter to run the script.
 is_init = 0
+init_default_levels()
 cases = {
     1: 'case1',
     2: 'case2',
